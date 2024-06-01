961aae9e29b63eb6804d52a5ad2e7d9dc801483e
NJ Development <nj.development83@gmail.com>
2024-06-01 01:10:42 -0300

    Created CoinImageService and LocalFileManager

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -11,26 +11,44 @@ import Combine
 
 final class CoinImageService {
     @Published var image: UIImage? = nil
+    
     private var imageSubscription: AnyCancellable?
     private let coin: Coin
+    private let fileManager = LocalFileManager.shared
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: Coin) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrived image from File Manager")
+        } else {
+            downloadCoinImage()
+            print("Downloading image")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(string: coin.image) else {
             return
         }
         
         imageSubscription = NetworkManager.download(url: url)
-            .tryMap({ data -> UIImage? in
-                return UIImage(data: data)
+            .tryMap({ data -> UIImage in
+                guard let image = UIImage(data: data) else { return UIImage() }
+                return image
             })
             .sink(receiveCompletion: NetworkManager.handleCompletion, receiveValue: { [weak self] image in
-                self?.image = image
-                self?.imageSubscription?.cancel()
+                guard let self else { return }
+                self.image = image
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
---
+++ CryptoTracker/Utilities/LocalFileManager.swift
@@ -0,0 +1,65 @@
+//
+//  LocalFileManager.swift
+//  CryptoTracker
+//
+//  Created by NJ Development on 01/06/24.
+//
+
+import Foundation
+import SwiftUI
+
+final class LocalFileManager {
+    static let shared = LocalFileManager()
+    private init() {}
+    
+    func saveImage(_ image: UIImage, imageName: String, folderName: String) {
+        // create folder
+        createFolderIfNeeded(folderName: folderName)
+        
+        // get path for image
+        guard let data = image.pngData(),
+              let url = getURLforImage(imageName: imageName, folderName: folderName)
+        else { return }
+        
+        // save path to image
+        do {
+            try data.write(to: url)
+        } catch {
+            print("Error saving image: \(error.localizedDescription)")
+        }
+    }
+    
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        guard let url = getURLforImage(imageName: imageName, folderName: folderName),
+                FileManager.default.fileExists(atPath: url.path   ) else {
+            return nil
+        }
+        return UIImage(contentsOfFile: url.path)
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLforFolder(folderName: folderName) else { return }
+        
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+            } catch {
+                print("Error creating directory \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    private func getURLforFolder(folderName: String) -> URL? {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return url.appendingPathComponent(folderName)
+    }
+    
+    private func getURLforImage(imageName: String, folderName: String) -> URL? {
+        guard let url = getURLforFolder(folderName: folderName) else {
+            return nil
+        }
+        return url.appendingPathComponent(imageName + ".png")
+    }
+}
