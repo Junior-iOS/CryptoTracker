bb792f5baca1342ad8bcc13c4f4c12ba170c7566
NJ Development <nj.development83@gmail.com>
2024-06-07 01:17:33 -0300

    Created StatisticView

--- CryptoTracker/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoTracker/Core/Home/ViewModel/HomeViewModel.swift
@@ -12,6 +12,12 @@ final class HomeViewModel: ObservableObject {
     @Published var allCoins: [Coin] = []
     @Published var portfolioCoins: [Coin] = []
     @Published var searchText: String = ""
+    @Published var statistics: [Statistic] = [
+        Statistic(title: "Title", value: "Value", percentageChange: 1),
+        Statistic(title: "Title", value: "Value"),
+        Statistic(title: "Title", value: "Value"),
+        Statistic(title: "Title", value: "Value", percentageChange: -7)
+    ]
     
     private var cancellables = Set<AnyCancellable>()
     private let service = CoinDataService()
@@ -21,11 +27,11 @@ final class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-//        service.$allCoins
-//            .sink { [weak self] coins in
-//                self?.allCoins = coins
-//            }
-//            .store(in: &cancellables)
+        //        service.$allCoins
+        //            .sink { [weak self] coins in
+        //                self?.allCoins = coins
+        //            }
+        //            .store(in: &cancellables)
         
         $searchText
             .combineLatest(service.$allCoins)
@@ -38,8 +44,8 @@ final class HomeViewModel: ObservableObject {
                 let lowercasedText = text.lowercased()
                 let filteredCoins = startingCoins.filter { coin in
                     return coin.id.lowercased().contains(lowercasedText) ||
-                           coin.name.lowercased().contains(lowercasedText) ||
-                           coin.symbol.lowercased().contains(lowercasedText)
+                    coin.name.lowercased().contains(lowercasedText) ||
+                    coin.symbol.lowercased().contains(lowercasedText)
                 }
                 
                 return filteredCoins
---
+++ CryptoTracker/Core/Home/Views/HomeStatsView.swift
@@ -0,0 +1,32 @@
+//
+//  HomeStatsView.swift
+//  CryptoTracker
+//
+//  Created by NJ Development on 07/06/24.
+//
+
+import SwiftUI
+
+struct HomeStatsView: View {
+    
+    @EnvironmentObject var homeViewModel: HomeViewModel
+    @Binding var showPortfolio: Bool
+    
+    var body: some View {
+        HStack {
+            ForEach(homeViewModel.statistics) { statistic in
+                StatisticView(stat: statistic)
+                    .frame(width: UIScreen.main.bounds.width / 3)
+            }
+        }
+        .frame(
+            width: UIScreen.main.bounds.width,
+            alignment: showPortfolio ? .trailing : .leading
+        )
+    }
+}
+
+#Preview {
+    HomeStatsView(showPortfolio: .constant(false))
+        .environmentObject(DeveloperPreview.shared.homeVM)
+}
