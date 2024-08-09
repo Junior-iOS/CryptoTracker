34d4958360493845e0f560d81cf27d36385eeb14
NJ Development <nj.development83@gmail.com>
2024-08-09 01:02:21 -0300

    Finished project

---
+++ CryptoTracker/Core/Settings/SectionView.swift
@@ -0,0 +1,18 @@
+//
+//  Sectionview.swift
+//  CryptoTracker
+//
+//  Created by NJ Development on 13/07/24.
+//
+
+import SwiftUI
+
+struct Sectionview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Sectionview()
+}
